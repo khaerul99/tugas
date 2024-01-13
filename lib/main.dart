a17b934f9af7e0a18e123f2f4db78c2e3f767dc2
khaerul99 <khaerul.r.23@gmail.com>
2024-01-13 18:55:05 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -28,12 +28,26 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Navbar(),
-      appBar: AppBar(
-        title: Text('Side Bar'),
+    return DefaultTabController(
+      length: 2, // Jumlah tab yang Anda inginkan
+      child: Scaffold(
+        drawer: Navbar(),
+        appBar: AppBar(
+          title: Text('Side Bar'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Tab 1'),
+              Tab(text: 'Tab 2'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: Text('Tab 1 Content')),
+            Center(child: Text('Tab 2 Content')),
+          ],
+        ),
       ),
-      body: Center(),
     );
   }
 }
